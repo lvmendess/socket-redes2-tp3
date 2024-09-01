6a238320ab27af1b22d7162497c910b3023e0abb
lvmendess <liviamendes1703@gmail.com>
2024-09-01 14:12:16 -0300

    comments

--- ClienteTCP.py
+++ ClienteTCP.py
@@ -4,7 +4,7 @@ serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName,serverPort))
 
-sentence = input('Digite "menu" para ver os produtos:')
+sentence = input('Digite "menu" para ver os produtos:') #mensagem inicial que solicita o menu
 clientSocket.send(sentence.encode())
 menu = clientSocket.recv(1024)
 print ('Resposta do servidor:', menu.decode())
@@ -16,25 +16,25 @@ print ('Resposta do servidor:', produto)
 
 if("produto encontrado" in produto):
     op = input ('Opções: comprar(1), fazer oferta(2), desistir(3). Digite o número da opção desejada:')
-    clientSocket.send(op.encode())
+    clientSocket.send(op.encode()) #envia opção para o servidor
     op = int(op)
     
     match op:
-        case 1:
+        case 1: #aceitou comprar
             response = clientSocket.recv(1024)
             print('Resposta do servidor:', response.decode())
-        case 2:
-            offers = 0
+        case 2: #negociar
+            offers = 0 #contador de ofertas
             response = ''
-            while(offers < 5):
+            while(offers < 5): #máximo 5 ofertas
                 offer = input('digite o valor que deseja pagar:')
                 clientSocket.send(offer.encode())
-                offers+=1
+                offers+=1 #aumenta o contador
                 response = clientSocket.recv(1024).decode()
                 print('Resposta do servidor:', response)
-                if('proposta aceita' in response):
+                if('proposta aceita' in response): #servidor aceitou a proposta
                     break;
-                else:
+                else: #servidor não aceitou a proposta e enviou contraproposta
                     ans = input('aceitar contraproposta (s/n)? ')
                     clientSocket.send(ans.encode())
                     if(ans == 's'):
@@ -42,7 +42,7 @@ if("produto encontrado" in produto):
                         print('Resposta do servidor:', reply)
                         exit();
             
-            if(offers>=5):
+            if(offers>=5): #cliente fez 5 ofertas e nenhuma foi aceita
                 print('limite de propostas atingido')
             else:
                 answer = clientSocket.recv(1024)
